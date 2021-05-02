8da1c42a7e824ae9233d363dc7b248e78d8402ba
tsrrodrigues <tsrrodrigues@outlook.com>
2021-05-02 14:09:40 -0300

    Corrige bug em getWord

--- src/utils/getWord.js
+++ src/utils/getWord.js
@@ -3,7 +3,8 @@ const palavras = require('../assets/palavras.json')
 
 const getWord = (nivel) => {
     nivel = parseInt(nivel);
-    if (nivel < 1 || nivel > 4) return false;
+    if (nivel < 1) return false;
+    if (nivel >= 4) nivel = 4;
     const max = palavras['nivel1'].length, min = 0;
     const indexPalavraAleatoria = Math.floor(Math.random() * (max - min) + min);
     const niveis = { 1: 'nivel1', 2: 'nivel2', 3: 'nivel3', 4: 'nivel4' };
